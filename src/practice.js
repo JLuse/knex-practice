1812b79cf525c8022e38a4adafd8209b19749d57
Jordan Luse <j.luse7@gmail.com>
2021-02-11 20:30:07 -0800

    Added knex practice

---
+++ src/practice.js
@@ -0,0 +1,99 @@
+require('dotenv').config();
+const knex = require('knex');
+
+// define knex first
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL
+})
+
+console.log('knex and driver installed correctly');
+
+// how to query in knex
+ const qry = knexInstance
+  .select('product_id', 'name', 'price', 'category')
+  .from('amazong_products')
+  .where({name: 'Point of view gun'})
+  // only select the first item found.
+  .first()
+  .toQuery()
+    // perform the query and log the results
+  // .then(results => {
+//   //   console.log(results);
+// })
+// console.log(qry);
+
+// knexInstance
+//   .select('product_id', 'name', 'price', 'category')
+//   .from('amazong_products')
+//   .where('name', 'ILIKE', `%${searchTerm}%`)
+//   .then(result => {
+//     // console.log(result)
+//   })
+
+//////// OFFSET, LIMIT
+// function paginateProducts(page) {
+//   const productsPerPage = 10;
+//   const offset = productsPerPage * (page - 1);
+
+//   knexInstance
+//     .select('product_id', 'name', 'price', 'category')
+//     .from('amazong_products')
+//     .limit(productsPerPage)
+//     .offset(offset)
+//     .then(result => {
+//       console.log(result)
+//     })
+// }
+
+// paginateProducts(2);
+
+//////// NOT NULL
+// function getProductsWithImages() {
+//   knexInstance
+//     .select('product_id', 'name', 'price', 'category', 'image')
+//     .from('amazong_products')
+//     .whereNotNull('image')
+//     .then(result => {
+//       console.log(result)
+//     })
+// }
+
+// getProductsWithImages()
+
+////// COUNT
+// function mostPopularVideosForDays(days) {
+//   knexInstance
+//     .select('video_name', 'region')
+//     .count('date_viewed AS views')
+//     .where(
+//       'date_viewed',
+//       '>',
+//       knexInstance.raw(`now() - '?? days'::INTERVAL`, days)
+//     )
+//     .from('whopipe_video_views')
+//     .groupBy('video_name', 'region')
+//     .orderBy([
+//       { column: 'region', order: 'ASC' },
+//       { column: 'views', order: 'DESC' },
+//     ])
+//     .then(result => {
+//       console.log(result)
+//     })
+// }
+
+// mostPopularVideosForDays(30)
+
+const searchTerm = 'ish';
+
+function getAllItems(searchTerm) {
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then(res => {
+      console.log(res)
+    })
+}
+
+getAllItems(searchTerm);
